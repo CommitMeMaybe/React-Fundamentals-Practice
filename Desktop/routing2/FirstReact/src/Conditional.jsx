9be56c170deabae64cb9a2cd09cc7413f43b4434
Klay <kelechiogonnaya@gmail.com>
2026-03-27 06:48:24 +0100

    cond, with ternary

--- Desktop/routing2/FirstReact/src/Conditional.jsx
+++ Desktop/routing2/FirstReact/src/Conditional.jsx
@@ -1,20 +1,38 @@
-function Conditional() {
-  const IsLoggedIn = false;
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Router, Link } from "react-router-dom";
 
-  //  Conditional with if else
-  if (IsLoggedIn === true) {
-    return (
-      <div>
-        <h1>User is Logged In Alright</h1>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <h1>User does not have access to this material</h1>
-      </div>
-    );
-  }
+function Conditional() {
+  const IsLoggedIn = true;
+  const studentsName = "Temi";
+  // Learnt useEffect
+  useEffect(
+    function () {
+      if (IsLoggedIn) {
+        alert("Logged In Sucessfuly");
+      } else {
+        alert("Please SignUp First");
+      }
+    },
+    [IsLoggedIn],
+  );
+
+  // Conditional with ternary operations
+  return (
+    <div>
+      <h1>Student Dashboard</h1>
+      <p>Welcome Back</p>
+
+      {IsLoggedIn ? (
+        <div>
+          <h1>Welcome back {studentsName} </h1>
+        </div>
+      ) : (
+        <div>
+          <p>Welcome Please Sign Up First</p>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Conditional;
